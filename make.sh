584837ab2b25c1db0951845d85862573dffd506c
Ando Emerencia <ando.emerencia@gmail.com>
2014-09-08 12:11:17 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sass src/scss/layout.scss:css/layout.css --style compressed
-find src/scripts/* -name "*.js" | xargs cat | uglifyjs -c -m -o js/app.min.js
+#find src/scripts/* -name "*.js" | xargs cat | uglifyjs -c -m -o js/app.min.js
